fa1ef2abee4811650cc74ba77d2349fcfcccd70d
omelendrez <omar.melendrez@gmail.com>
2020-03-28 19:25:59 -0300

    Let's use bootstrap

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="alert alert-primary">
+        Esta es una nueva página
+      </div>
+      <button className="btn btn-primary">Dar Enter</button>
     </div>
   );
 }
