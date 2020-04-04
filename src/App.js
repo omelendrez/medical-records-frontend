b0c417dbbfd754c4453520f01471e90003959bcf
omelendrez <omar.melendrez@gmail.com>
2020-04-04 18:33:41 -0300

    Agregar Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import Navbar from './components/Navbar'
 import Customers from './components/Customers'
 
 function App() {
   return (
     <div className="container-fluid">
+      <Navbar />
       <Customers />
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <span className="navbar-brand">Historias Clínicas</span>
+    </nav>
+  )
+}
+
+export default Navbar
