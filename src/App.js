8020e478134cb917aeaffb976ab827af4320fbf4
omelendrez <omar.melendrez@gmail.com>
2020-04-05 00:14:59 -0300

    Agregar imagen y rutas

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
 import Customers from './components/Customers'
 import Pets from './components/Pets'
 
 function App() {
   return (
-    <div className="container-fluid">
+    <BrowserRouter>
       <Navbar />
-      <Customers />
-      <Pets />
-    </div>
+      <main>
+        <Switch>
+          <Route path="/" component={Home} exact />
+          <Route path="/clientes" component={Customers} exact />
+          <Route path="/pacientes" component={Pets} />
+        </Switch>
+      </main>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 
 const Navbar = () => {
+  const location = useLocation()
+  const isSeacrh = location.pathname === '/clientes'
+
   return (
-    <nav className="navbar navbar-dark bg-primary">
-      <span className="navbar-brand">Historias Clínicas</span>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/clientes">Clientes</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/pacientes">Pacientes</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/consultas">Consultas</a>
+          </li>
+        </ul>
+        {isSeacrh &&
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" aria-label="Search" />
+            <button className="btn btn-warning" type="submit">Buscar</button>
+          </form>}
+
+      </div>
     </nav>
   )
 }
