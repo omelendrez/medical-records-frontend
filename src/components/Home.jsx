df58602503da64d41ee8d6884b753618b5a893a7
omelendrez <omar.melendrez@gmail.com>
2020-04-10 18:11:45 -0300

    Agregar logo

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,9 +4,8 @@ import './Home.css'
 const Home = () => {
   return (
     <div className="image">
-      <h1>
-        Veterinaria Colitas Felices
-      </h1>
+      <div className="logo">
+      </div>
     </div>
   )
 }
