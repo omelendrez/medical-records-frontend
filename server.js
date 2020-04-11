37f229aec676c1edbc183daeb60dd1ee58034592
omelendrez <omar.melendrez@gmail.com>
2020-04-11 17:26:18 -0300

    Redireccionar server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const app = express()
 
-app.use(serveStatic(__dirname + '/dist'))
+app.use(serveStatic(__dirname + '/build'))
 
 const port = process.env.PORT || 5000
 app.listen(port)
