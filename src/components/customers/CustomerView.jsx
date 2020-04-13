1c8f3e72024f4e77d89da710a59e650cbda8783a
omelendrez <omar.melendrez@gmail.com>
2020-04-13 19:22:40 -0300

    Refactorización de vista cliente

--- src/components/customers/CustomerView.jsx
+++ src/components/customers/CustomerView.jsx
@@ -1,11 +1,110 @@
 import React, { useState, useEffect } from 'react'
-import { Redirect, Link } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
 import { getCustomer } from '../../services/customers'
 import { getPet } from '../../services/pets'
-import Pet from './Pet'
 import './CustomerView.css'
 
-const CustomerForm = props => {
+const Consultation = ({ consultation }) => {
+  const { date, diagnosis, treatment, nextConsultation, observations } = consultation
+  return (
+    <div className="consultation">
+      <div>{date}</div>
+      <div>{diagnosis}</div>
+      <div>{treatment}</div>
+      <div>{nextConsultation}</div>
+      <div>{observations}</div>
+    </div>
+  )
+}
+
+const Consultations = ({ consultations }) => {
+  return (
+    <div className="card consultations">
+      {consultations.map(consultation => <Consultation consultation={consultation} />)}
+    </div>
+  )
+}
+
+const Pet = ({ pet }) => {
+  const { name, type, breed, observations } = pet
+  return (
+    <div>
+      <div>
+        <h5 className="card-title">{name}</h5>
+      </div>
+      <div>
+        {type}
+      </div>
+      <div>
+        {breed}
+      </div>
+      <div>
+        {observations}
+      </div>
+    </div>
+  )
+}
+
+const Customer = ({ customer, pet, handleAddPet, selectPet, setBack }) => {
+  const { name, address, phone, observations, pets } = customer
+  return (
+    <div className="card customer">
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{address}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">{phone}</h6>
+
+        <p className="card-text observations">{observations}</p>
+
+        {!pet.name && <div className="pets">
+          <div className="pets-header">
+            Pacientes
+        </div>
+          <ul className="list-group">
+            {
+              pets.map((pet, index) => {
+                return (
+                  <li
+                    className="list-group-item"
+                    key={index}
+                    onClick={() => selectPet(pet)}
+                  >
+                    {pet.name}
+                  </li>
+                )
+              })
+            }
+          </ul>
+
+
+          <div>
+            <button
+              type="button"
+              className="btn btn-primary mt-4"
+              onClick={e => handleAddPet(e)}
+            >Agregar</button>
+          </div>
+        </div>
+        }
+        {pet.name && <Pet pet={pet} />}
+        {
+          !customer.pets.length && <div className="alert alert-warning my-3">No tiene mascotas</div>
+        }
+
+      </div>
+      <div className="container-fluid my-4">
+        <button
+          type="button"
+          className="btn btn-danger float-right"
+          onClick={() => setBack(true)}
+        >Volver</button>
+      </div>
+    </div>
+
+  )
+}
+
+const CustomerView = props => {
   const [back, setBack] = useState(false)
   const [addPet, setAddPet] = useState('')
   const [customer, setCustomer] = useState({ pets: [] })
@@ -25,72 +124,26 @@ const CustomerForm = props => {
     getPet(pet.id)
       .then(pet => setPet(pet))
   }
-
+  const { consultations } = pet
   return (
     <>
       {addPet && <Redirect to={addPet} />}
       {back && <Redirect to="/clientes" />}
       <div className="main-container">
+        <Customer
+          customer={customer}
+          pet={pet}
+          handleAddPet={handleAddPet}
+          selectPet={selectPet}
+          setBack={setBack}
+        />
 
-        <div className="card customer">
-          <div className="card-body">
-            <h5 className="card-title">{customer.name}</h5>
-            <h6 className="card-subtitle mb-2 text-muted">{customer.address}</h6>
-            <h6 className="card-subtitle mb-2 text-muted">{customer.phone}</h6>
+        {pet.name && <Consultations consultations={consultations} />}
 
-            <p className="card-text observations">{customer.observations}</p>
-
-            {!pet.name && <div className="pets">
-              <div className="pets-header">
-                Pacientes
-              </div>
-              <ul className="list-group">
-                {
-                  customer.pets.map((pet, index) => {
-                    return (
-                      <li
-                        className="list-group-item"
-                        key={index}
-                        onClick={() => selectPet(pet)}
-                      >
-                        {pet.name}
-                      </li>
-                    )
-                  })
-                }
-              </ul>
-
-
-              <div>
-                <button
-                  type="button"
-                  className="btn btn-primary mt-4"
-                  onClick={e => handleAddPet(e)}
-                >Agregar</button>
-              </div>
-            </div>
-            }
-            {pet.name && <Pet pet={pet} />}
-            {
-              !customer.pets.length && <div className="alert alert-warning my-3">No tiene mascotas</div>
-            }
-
-          </div>
-          <div className="container-fluid my-4">
-            <button
-              type="button"
-              className="btn btn-danger float-right"
-              onClick={() => setBack(true)}
-            >Volver</button>
-          </div>
-        </div>
-        {pet.name && <div className="card consultations">
-          {pet.consultations.map(consultation => consultation.treatment).join('<br/>')}
-        </div>}
       </div>
 
     </>
   )
 }
 
-export default CustomerForm
+export default CustomerView
