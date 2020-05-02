ce6a131c4ae24a2890e03c01d3186b9006f3bd5c
Omar Melendrez <omelendrez@bfr.com>
2020-05-02 10:42:30 -0500

    Conectar con base de datos en la nube

--- src/services/api.js
+++ src/services/api.js
@@ -5,5 +5,5 @@ export default axios.create({
     'Accept': 'application/json',
     'Content-Type': 'application/json'
   },
-  baseURL: 'http://localhost:3000/api'
+  baseURL: 'https://vmr.herokuapp.com/api'
 })
