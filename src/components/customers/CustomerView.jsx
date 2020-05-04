20834f814c7fbad8fd053ed73f72d84231268d32
Ema <emarengo73@gmail.com>
2020-05-04 19:52:23 -0300

    Mostrar sexo, nacimiento y peso

--- src/components/customers/CustomerView.jsx
+++ src/components/customers/CustomerView.jsx
@@ -84,13 +84,16 @@ const Consultations = ({ consultations, addConsultation, editConsultation, delet
 }
 
 const Pet = ({ pet }) => {
-  const { name, type, breed, observations, statusId } = pet
+  const { name, type, breed, sex, weight, yearBorn, observations, statusId } = pet
   return (
     <div className="card pet">
       <div className="card-body">
         <h5 className="card-title">{name}</h5>
         <h6 className="card-subtitle mb-2 text-muted">{type}</h6>
         <h6 className="card-subtitle mb-2 text-muted">{breed}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">{sex}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">{weight}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">{yearBorn}</h6>
         <p className="card-text observations">{observations}</p>
         <p className={`status ${statusId === 1 ? 'active' : 'inactive'}`}>{statusId === 1 ? 'Activo' : 'Inactivo'}</p>
       </div>
