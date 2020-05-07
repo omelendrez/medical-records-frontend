54a0fe0c7b912c14219e6383f485bafcc32ba800
Omar Melendrez <omelendrez@bfr.com>
2020-05-07 15:35:56 -0500

    Agregar cliente a lista de consultas

--- src/components/consultations/Consultation.jsx
+++ src/components/consultations/Consultation.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 const Consultation = ({ data, deleteConsultation, editConsultation }) => {
 
-  const { date, diagnosis, treatment, nextConsultation, petId, pet } = data
+  const { date, diagnosis, treatment, nextConsultation, petId, customerId, pet } = data
 
   return (
     <tr>
@@ -11,20 +11,20 @@ const Consultation = ({ data, deleteConsultation, editConsultation }) => {
         {date}
       </td>
       <td>
-        <Link to={`/clientes/${pet.customer.id}/${petId}`}>
+        <Link to={`/clientes/${customerId}/${petId}`}>
           {pet.name}
         </Link>
       </td>
-      <td>{diagnosis.substring(0,40)}</td>
-      <td>{treatment.substring(0,60)}</td>
+      <td>{diagnosis.substring(0, 40)}</td>
+      <td>{treatment.substring(0, 60)}</td>
       <td className="text-nowrap">{nextConsultation}</td>
-      <td style={{width: '120px'}}>
+      <td style={{ width: '120px' }}>
         <button
           className="btn btn-danger"
           onClick={() => deleteConsultation(data)}
         >Eliminar</button>
       </td>
-      <td style={{width: '120px'}}>
+      <td style={{ width: '120px' }}>
         <button
           className="btn btn-info"
           onClick={() => editConsultation(data)}
