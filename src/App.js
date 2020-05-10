534ea4fe7775f749e3d9a51693da99161bac2fe2
omelendrez <omar.melendrez@gmail.com>
2020-05-10 19:21:09 -0300

    Eliminar busqueda del menu de tareas

--- src/App.js
+++ src/App.js
@@ -16,27 +16,25 @@ import ConsultationEdit from './components/consultations/ConsultationEdit'
 import Restore from './components/Restore'
 
 function App() {
-  const [filter, setFilter] = useState('')
-
   return (
     <BrowserRouter>
-      <Navbar doSearch={setFilter} />
+      <Navbar />
       <main>
         <Switch>
           <Route path="/" component={Home} exact />
-          <Route path="/clientes" exact component={() => <Customers filter={filter} />} />
+          <Route path="/clientes" exact component={() => <Customers />} />
           <Route path="/clientes/:id" exact component={CustomerView} />
           <Route path="/clientes/:id/:petId" exact component={CustomerView} />
           <Route path="/nuevo-cliente" component={CustomerAdd} />
           <Route path="/edit-cliente/:id" exact component={CustomerEdit} />
-          <Route path="/pacientes" exact component={() => <Pets filter={filter} />} />
+          <Route path="/pacientes" exact component={() => <Pets />} />
           <Route path="/nuevo-paciente/:id" exact component={PetAdd} />
           <Route path="/edit-paciente/:id" exact component={PetEdit} />
-          <Route path="/consultas" component={() => <Consultations filter={filter} />} />
+          <Route path="/consultas" component={() => <Consultations />} />
           <Route path="/edit-consulta/:consultationId" exact component={ConsultationEdit} />
           <Route path="/nueva-consulta/:customerId/:petId" exact component={ConsultationAdd} />
           <Route path="/restaurar/:table" exact component={Restore} />
-          <Route path="/deudores" exact component={() => <Debtors filter={filter} />} />
+          <Route path="/deudores" exact component={() => <Debtors />} />
         </Switch>
       </main>
     </BrowserRouter>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,10 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { useLocation } from 'react-router-dom'
 import NavLink from './NavLink'
 
-const Navbar = ({ doSearch }) => {
+const Navbar = () => {
   const { pathname: page } = useLocation()
   const hasSearch = page === '/clientes' || page === '/pacientes' || page === '/consultas' || page === '/deudores'
-
-  const [search, setSearch] = useState('')
-
-  const onChange = e => {
-    setSearch(e.target.value)
-  }
-
-  const onClick = (e) => {
-    e.preventDefault()
-    doSearch(search)
-  }
 
   return (
     <nav className="navbar navbar-expand navbar-dark bg-dark">
@@ -27,21 +16,6 @@ const Navbar = ({ doSearch }) => {
           <NavLink to="/consultas">Consultas</NavLink>
           <NavLink to="/deudores">Deudores</NavLink>
         </ul>
-        {
-          hasSearch &&
-          <form className="form-inline my-2 my-lg-0">
-            <input
-              className="form-control mr-sm-2"
-              type="search"
-              aria-label="Search"
-              onChange={e => onChange(e)}
-            />
-            <button
-              className="btn btn-warning"
-              onClick={e => onClick(e)}
-            >Buscar</button>
-          </form>
-        }
       </div>
     </nav>
   )
