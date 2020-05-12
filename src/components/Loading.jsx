13cd8ddfda29f3c29a5f6d7c28ab48d75f3ac208
omelendrez <omar.melendrez@gmail.com>
2020-05-11 23:19:19 -0300

    Agregar atributo alt a la imagen de loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -5,7 +5,7 @@ import './Loading.css'
 const Loading = () => {
   return (
     <div className="d-flex justify-content-center loading">
-      <img src={img} />
+      <img src={img} alt='loading...' />
     </div>
   )
 }
