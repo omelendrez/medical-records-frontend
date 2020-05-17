4767b9718ccab74916f37bdc5e08a3b6d1c55449
matimancinelli <matiasmancinelli1998@hotmail.com>
2020-05-17 18:36:01 -0300

    Resolve #113

--- src/components/customers/customer-view/Consultation.jsx
+++ src/components/customers/customer-view/Consultation.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import Balance from './Balance'
 
 const Consultation = ({ consultation, editConsultation, deleteConsultation }) => {
-  const { id, date, clinicalExamination, diagnosis, treatment, nextConsultation, observations, amount, paid } = consultation
+  const { id, date, clinicalExamination, diagnosis, treatment, nextConsultation, anamnesis, amount, paid } = consultation
 
   return (
     <div className="card consultation">
       <div className="card-body">
         {amount > 0 && <Balance amount={amount} paid={paid} />}
         <h6 className="card-title">{date}</h6>
+        {anamnesis && <p className="card-text anamnesis ">{anamnesis}</p>}
         {clinicalExamination && <h6 className="card-subtitle mb-2">Examen Clinico: {clinicalExamination}</h6>}
         {diagnosis && <h6 className="card-subtitle mb-2">Diagnóstico: {diagnosis}</h6>}
         {treatment && <p className="card-text texts">Tratamiento: {treatment}</p>}
         {nextConsultation && <h6 className="card-subtitle mb-2">Próxima consulta: {nextConsultation}</h6>}
-        {observations && <p className="card-text observations">{observations}</p>}
         <div>
           <button
             type="button"
