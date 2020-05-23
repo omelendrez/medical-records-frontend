1aa3aaa6e9c95432adfdc33882e87e230e4fa589
omelendrez <omar.melendrez@gmail.com>
2020-05-23 18:04:21 -0300

    Cambiar componente financiero

--- src/components/customers/customer-view/Amount.jsx
+++ src/components/customers/customer-view/Amount.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const Amount = ({ text, value }) => {
-  return <div className="amount-row mx-4">
-    <div className="amount-label">{text}</div>
+  return <div className="amount-row mx-2">
+    <div className="amount-label mr-4">{text}:</div>
     <div className="amount">{value}</div>
   </div>
 }
