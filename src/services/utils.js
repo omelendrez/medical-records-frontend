c15010af3280e262a696ebeccf57e6467961f62a
matimancinelli <matiasmancinelli1998@hotmail.com>
2020-05-25 18:14:06 -0300

    Resolve #142

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,6 +1,7 @@
 import { getInactiveCustomers, restoreCustomer } from '../services/customers'
 import { getInactivePets, restorePet } from '../services/pets'
 import { getInactiveConsultations, restoreConsultation } from '../services/consultations'
+import { getInactiveVaccinations, restoreVaccination } from '../services/vaccinations'
 import moment from 'moment'
 import 'moment/locale/es'
 
@@ -25,6 +26,16 @@ export const fieldsDefault = {
     ],
     getRecords: getInactivePets,
     restoreRecord: restorePet
+  },
+  vacunaciones: {
+    fields: [
+      { name: 'date', title: 'Fecha', className: "text-nowrap" },
+      { name: 'petName', title: 'Paciente' },
+      { name: 'vaccination', title: 'Desparasitación' },
+      { name: 'nextAppointment', title: 'Próx. Turno', className: "text-nowrap" }
+    ],
+    getRecords: getInactiveVaccinations,
+    restoreRecord: restoreVaccination
   },
   consultas: {
     fields: [
