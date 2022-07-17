675c92b75e937ffb77cb570abb2964b6bd1f3d33
Omar Melendrez <omar.melendrez@gmail.com>
2022-07-17 19:18:23 -0300

    Remove repotWebVitals, serviceWorker and fix a couple of small issues

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react"
-import NavLink from "./NavLink"
-import { logout, getUser, isReadOnly, isAdmin } from "../services/utils"
+import React, { useEffect, useState } from 'react'
+import NavLink from './NavLink'
+import { logout, getUser, isReadOnly, isAdmin } from '../services/utils'
 
 const Navbar = () => {
   const [user, setUser] = useState({})
@@ -19,38 +19,38 @@ const Navbar = () => {
   const windowResize = () => setIsSmallDevice(window.innerWidth < 768)
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
       <button
-        className="navbar-toggler collapsed"
-        type="button"
-        data-toggle={isSmallDevice ? "collapse" : undefined}
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
+        className='navbar-toggler collapsed'
+        type='button'
+        data-toggle={isSmallDevice ? 'collapse' : undefined}
+        data-target='#navbarSupportedContent'
+        aria-controls='navbarSupportedContent'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
       >
-        <span className="navbar-toggler-icon"></span>
+        <span className='navbar-toggler-icon'></span>
       </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <div className='collapse navbar-collapse' id='navbarSupportedContent'>
         <ul
-          className="navbar-nav mr-auto"
-          data-toggle={isSmallDevice ? "collapse" : undefined}
-          data-target="#navbarSupportedContent"
+          className='navbar-nav mr-auto'
+          data-toggle={isSmallDevice ? 'collapse' : undefined}
+          data-target='#navbarSupportedContent'
         >
-          <NavLink to="/">Home</NavLink>
-          <div className="dropdown-divider"></div>
-          <NavLink to="/turnos">Turnos</NavLink>
-          <NavLink to="/clientes">Clientes</NavLink>
-          <NavLink to="/pacientes">Pacientes</NavLink>
-          <NavLink to="/consultas">Consultas</NavLink>
-          <NavLink to="/vacunaciones">Vacunaciones</NavLink>
-          <NavLink to="/desparasitaciones">Desparasitaciones</NavLink>
-          {!disabled && <NavLink to="/deudores">Deudores</NavLink>}
-          {isAdmin() && <NavLink to="/users">Users</NavLink>}
+          <NavLink to='/'>Home</NavLink>
+          <div className='dropdown-divider'></div>
+          <NavLink to='/turnos'>Turnos</NavLink>
+          <NavLink to='/clientes'>Clientes</NavLink>
+          <NavLink to='/pacientes'>Pacientes</NavLink>
+          <NavLink to='/consultas'>Consultas</NavLink>
+          <NavLink to='/vacunaciones'>Vacunaciones</NavLink>
+          <NavLink to='/desparasitaciones'>Desparasitaciones</NavLink>
+          {!disabled && <NavLink to='/deudores'>Deudores</NavLink>}
+          {isAdmin() && <NavLink to='/users'>Users</NavLink>}
         </ul>
-        <div className="text-white mr-2 text-capitalize">{user.name}</div>
+        <div className='text-white mr-2 text-capitalize'>{user.name}</div>
         <button
-          className="btn btn-warning btn-sm"
+          className='btn btn-warning btn-sm'
           onClick={(e) => handleLogout(e)}
         >
           Logout
